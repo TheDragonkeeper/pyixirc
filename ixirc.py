c68ff0527c3b89bfe9d09064b71187d427b19f09
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2015-12-05 09:01:37 +0000

    added bot irc and !s,!d,!q commands

--- ixirc.py
+++ ixirc.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
-"""The script searches ixirc.com and parases
-the simple json output"""
+# -*- coding: utf8 -*-
 
+import socket, string, time, ssl
+import urllib.request, urllib.parse, urllib.error, re
 import sys
 import json
 import requests
 import argparse
 
+network = 'irc.abjects.net'  ##last run,  wouldnt connect to abject, will to freenode:7000 ssl
+nick = 'nottly'
+chan = 'moviegods'
+port = 9999
 
+socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+searchterm = '0'  ###seems to stick at this value and not redefine in while loop
+chanid = ''
+page = ''
+reverse = ''
 class termcolors:
     """Asign some cheap color output for the shell."""
     HEADER = '\033[95m'
@@ -20,48 +31,12 @@ class termcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
-def get_args():
-    """get all args"""
-    parser = argparse.ArgumentParser(
-        description='Simple cli search for ixirc',
-        epilog="Made by shaggy")
-    parser.add_argument(
-        '-V', '-v', '--version', action='version',
-        version='%(prog)s ' + "0.1")
-    parser.add_argument(
-        '-s', '--searchterm', type=str, help='search string',
-        required=False, default=None)
-    parser.add_argument(
-        '-c', '--chanid', type=str, help='channel id, see list:',
-        required=False, default=None)
-    parser.add_argument(
-        '-r', '--reverse', action='store_true', help='reverse order',
-        required=False)
-    parser.add_argument(
-        '-p', '--page', type=str, help='page number',
-        required=False)
-    args = parser.parse_args()
-    searchterm = args.searchterm
-    chanid = args.chanid
-    page = args.page
-    reverse = args.reverse
-    #conflictdate = args.chanid, args.page
-    #if all(conflictdate):
-    #    sys.exit('Conflict in options: can not use \
-    #            page option with chanid.')
-    return searchterm, chanid, page, reverse
-
-searchterm, chanid, page, reverse = get_args()
-
-
 def do_search():
     searches = requests.get("http://ixirc.com/api/?q=%s&cid=%s&pn=%s" % (
         searchterm, chanid, page)).json()
     jsondata = json.loads(str(searches).replace("'", '"'))
     jsons = jsondata['results']
     return jsons
-
 
 def print_search():
     for item in reversed(do_search()):
@@ -76,8 +51,7 @@ def print_search():
         print(termcolors.RED + size + termcolors.YELLOW + " /msg",
               botn, "xdcc send", packn, termcolors.ENDC)
         print("---------------------------------------------")
-
-
+    print(searchterm)
 def print_pageinfo():
     for item in do_search():
         results = item['c']
@@ -86,12 +60,36 @@ def print_pageinfo():
               termcolors.BLUE + "on total of ", pagecount + termcolors.ENDC)
         print("---------------------------------------------")
 
+def main(network, nick, chan, port):
+	socket.connect((network,port))
+	irc = ssl.wrap_socket(socket)
+	irc.send(bytes('NICK %s\r\n' % nick, "UTF-8"))
+	print(irc.recv(4096))
+	irc.send(bytes('USER %s %s %s :My bot\r\n' % (nick,nick,nick), "UTF-8"))
+	print(irc.recv(4096))
+	irc.send(bytes('JOIN #%s\r\n' % chan, "UTF-8"))
+	print(irc.recv(4096))
 
-def main():
-    try:
-        print_search()
-    except KeyError:
-        print("No Result Found")
-
-if __name__ == '__main__':
-    main()
+	while True:
+		data = irc.recv(4096)
+		print(data)
+		user_in = input(":")
+		if user_in.find("!s") != -1:
+			user_in = user_in[3:]
+			searchterm = user_in  ##this isnt redefining for use in print_search 
+			try:
+				print_search()   ## results show but needs searchterm to work to be specific
+			except KeyError:
+				print("No Result Found")
+		print(user_in)
+		data = data.decode("UTF-8")
+		if data.find("PING") != -1:
+			irc.send(bytes('PONG '+data.split()[1]+'\r\n', "UTF-8"))
+		if user_in.find('!q') != -1:
+			irc.send(bytes('QUIT\r\n', "UTF-8"))
+			exit()
+#		if user_in.find('!d') != -1:   ##to check for xfer
+			#user_in = user_in[3:]   ## for option select  !d 1  will show as 1
+#			irc.send(bytes('PRIVMSG bot_name :xdcc send pack_number\r\n', "UTF-8"))
+if __name__=='__main__':
+    main(network, nick, chan, port)
